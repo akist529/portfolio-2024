b4128316de410cf83f734b80b6e368ac97231633
akist529 <akist0707@gmail.com>
2024-02-25 23:52:13 -0600

    Update transition component to use swipe symbol for mobile and tablet displays

--- app/components/transition/transition.tsx
+++ app/components/transition/transition.tsx
@@ -1,12 +1,27 @@
 import { BiSolidMouse } from "react-icons/bi";
-import { MdArrowDownward } from "react-icons/md";
+import { MdArrowDownward, MdOutlineSwipeDown } from "react-icons/md";
 import styles from './transition.module.scss';
+import { WindowContext } from "@/app/page";
+import { useContext, useMemo } from "react";
 
 export default function Transition () {
+    const window = useContext(WindowContext);
+
+    const isMobile = useMemo(() => {
+        if (window.width >= 1120) {
+            return false;
+        } else {
+            return true;
+        }
+    }, [window]);
+
     return (
         <div className={styles.transition}>
-            <BiSolidMouse size={64} />
-            <MdArrowDownward size={64} />
+            { isMobile && <MdOutlineSwipeDown size={64} /> }
+            { !isMobile && <>
+                <BiSolidMouse size={64} />
+                <MdArrowDownward size={64} />
+            </> }
         </div>
     );
 }
