7f4b4d222a67ecef5435728c9e81f6f0cf1e86c2
akist529 <akist0707@gmail.com>
2024-02-27 01:44:59 -0600

    Add proper default values for contexts to satisfy TypeScript

--- app/context.tsx
+++ app/context.tsx
@@ -1,4 +1,11 @@
 import { createContext } from "react";
 
-export const DarkContext = createContext({});
-export const WindowContext = createContext({});
+export const DarkContext = createContext({
+    darkMode: false,
+    setDarkMode: () => {}
+});
+
+export const WindowContext = createContext({
+    width: 0,
+    setWidth: () => {}
+});
